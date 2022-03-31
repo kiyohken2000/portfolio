702894e73f10acfdd6965a8f517b0c5cd42f9171
*D8uZD3ENJO <shirakura@colors.jp>
2022-03-31 20:47:32 +0900

    master

--- src/mock/data.js
+++ src/mock/data.js
@@ -59,8 +59,8 @@ export const projectsData = [
     title: 'React Native Expo with Firebase',
     info: 'React Narive Expo boilerplate. It uses Firebase for the backend, and supports account creation, image upload, get and save push notifications token, and dark theme.',
     info2: 'Firebase用のReact Native Expoのボイラープレートです。アカウントの作成とユーザー情報の更新、画像のアップロード、プッシュ通知トークンの取得と保存、ダークモードをサポートしています。',
-    url: 'https://github.com/kiyohken2000/reactnative-expo-firebase-boilerplate',
-    repo: 'https://github.com/kiyohken2000/reactnative-expo-firebase-boilerplate', // if no repo, the button will not show up
+    url: 'https://github.com/kiyohken2000/ReactNative-Expo-Firebase-Boilerplate-v2',
+    repo: 'https://github.com/kiyohken2000/ReactNative-Expo-Firebase-Boilerplate-v2', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -97,25 +97,7 @@ export const projectsData = [
     info2: 'シンプルスタイルニュースリーダーアプリです。様々なニュースやまとめサイトを、広告を排除したシンプルかつ統一されたフォーマットで閲覧することができます。ウェブビューではなくネイティブビューでレンダリングしているため、快適なブラウジング体験を実現しました。',
     url: 'https://kenmo-reader.ml/',
     repo: 'https://github.com/kiyohken2000/kenmo-reader', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'retwpay.png',
-    title: 'Retwpay',
-    info: 'Retwpay is Retweet Flea Market App. Retwpay helps matching that buy and sell of Retweet.',
-    info2: 'リツイートフリマアプリです。リツイートを「買いたい」と「売りたい」のマッチングを支援します。ユーザー同士のコミュニケーションにはツイッターを、決済には様々な電子マネーの個人間決済を利用します。リンク先のランディングページでは開発ブログも投稿しています。',
-    url: 'https://retwpay.ml/',
-    repo: '', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'votepurchase.png',
-    title: 'Votepurchase',
-    info: 'This is the first product I made. This is the web application version of Retwpay.',
-    info2: '私が初めて作った製品です。Retwpayのウェブアプリ版です。',
-    url: 'https://votepurchase.net/',
-    repo: '', // if no repo, the button will not show up
-  },
+  }
 ];
 
 // CONTACT DATA
